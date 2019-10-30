0bfa6096b0f9fb83e318b202f3529f07e19d344c
Alpha Olomi <alphaolomi@gmail.com>
2019-10-31 00:40:13 +0300

    tests: add null test

--- test/test.js
+++ test/test.js
@@ -1,11 +1,14 @@
 var simple = require('../lib/script');
 var expect = require('expect.js');
 
-describe("simple", function(){
-  it("should return true", function(){
+describe('simple', function() {
+  it('should return true', function() {
     expect(simple.math(1)).to.equal(true);
   });
-  it("should return false", function(){
+  it('should return false', function() {
     expect(simple.math(0)).to.equal(false);
+  });
+  it('should return null', function() {
+    expect(simple.math(2)).to.null;
   });
 });
